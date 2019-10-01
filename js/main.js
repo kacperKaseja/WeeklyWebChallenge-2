bff07f1de662849c714b2e8e9bcb38bcb76a3dde
Kacper Kaseja <yamakaszi10@o2.pl>
2019-10-01 14:17:42 +0200

    Add servuce section whit and scrol function in js

--- js/main.js
+++ js/main.js
@@ -14,9 +14,38 @@ hamburgerBtn.addEventListener('click', hamburgerTransform)
 const navLinks = [...document.querySelectorAll('.menu__navigation')];
 
 function navSelection(e) {
-    e.preventDefault();
+    // e.preventDefault();
     console.log('adsasdas');
     this.classList.add('.border-top');
+    menu.classList.remove('menu--active');
+    hamburgerBtn.classList.toggle('hamburger--active');
 }
-
+// scroll function
 navLinks.forEach(link => link.addEventListener('click', navSelection))
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            block: 'start',
+            behavior: 'smooth',
+        });
+    });
+
+});
+
+
+
+
+// let mainNavLinks = document.querySelectorAll("nav ul li a");
+
+// mainNavLinks.forEach(link => {
+//     link.addEventListener("click", event => {
+//         event.preventDefault();
+//         let target = document.querySelector(event.target.hash);
+//         target.scrollIntoView({
+//             behavior: "smooth",
+//             block: "start"
+//         });
+//     });
+// });
